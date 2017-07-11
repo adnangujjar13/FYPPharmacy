693c36e7631b23cf6a0a379b26e7407e8904243b
Adnan Gujjar <adnan.gujjar13@gmail.com>
2017-07-11 16:35:47 +0500

    initial

--- app/src/main/java/com/example/adnangujjar/fyp/ApiClient.java
+++ app/src/main/java/com/example/adnangujjar/fyp/ApiClient.java
@@ -8,6 +8,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
+    //h
     public static final String BASE_URL = "http://192.168.43.219/";
     private static Retrofit retrofit = null;
     //public static int unique_id;
